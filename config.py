27ab92935117bfd77511edb55515f4d5879800c0
Yoshitomo Minami <minami530@gmail.com>
2015-12-30 22:04:43 +0900

    設定ファイルを追加

---
+++ config.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# write code...
+
+# mongodb
+HOST = "localhost"
+PORT = 27017
+DB_NAME = "twitter-archive"
+COLLECTION_NAME = "tweets"
+
+# twitter
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN_KEY = ""
+ACCESS_TOKEN_SECRET = ""
