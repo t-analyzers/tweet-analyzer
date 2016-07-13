e30ed359d4f3b6e842d2ecd5c7aa9fc326628952
古川 人士 <hitoshi.furukawa@gmail.com>
2016-07-13 23:21:02 +0900

    個人の設定情報を追加。

--- analyzer/config.py
+++ analyzer/config.py
@@ -8,10 +8,10 @@ DB_NAME = "twitter-archive"
 COLLECTION_NAME = "tweets"
 
 # twitter
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN_KEY = ""
-ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = "hbHAeRIwnXjFeCaG5dxeQY2IG"
+CONSUMER_SECRET = "dnYsrbdRl2wxDlKkSV70qWZoM7PANlv9EDL884GCuy72mTpO39"
+ACCESS_TOKEN_KEY = "615963673-vS0rtz54V4UxMeeWLq6EGi3vAO3fuN4cQUBfVHLe"
+ACCESS_TOKEN_SECRET = "sgUYRV7o9nD3dr4PzZ7wYc14n9BNpqzCUtqNjA7gKKNZP"
 
 # The full url to an HTTPS proxy to use for connecting to Twitter.
 PROXY = ""
