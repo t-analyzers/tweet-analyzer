6df53c8cc007f62ec1fccc0d4cd1160e37d39e0d
furuhit0519 <programming.adelie.penguin@gmail.com>
2016-07-31 21:32:22 +0900

    twitterのkey,tokenをregenerate。その他パラメータも初期値に更新。

--- analyzer/config.py
+++ analyzer/config.py
@@ -8,10 +8,10 @@ DB_NAME = "twitter-archive"
 COLLECTION_NAME = "tweets"
 
 # twitter
-CONSUMER_KEY = "42ckSu4sLnYCLlzcLnQ0rEF6U"
-CONSUMER_SECRET = "VgKmmr1cJYjp6oWRIbQiV85cFIE494TdXRN0Gc6ebdkzNnpl7N"
-ACCESS_TOKEN_KEY = "615963673-vS0rtz54V4UxMeeWLq6EGi3vAO3fuN4cQUBfVHLe"
-ACCESS_TOKEN_SECRET = "sgUYRV7o9nD3dr4PzZ7wYc14n9BNpqzCUtqNjA7gKKNZP"
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN_KEY = ""
+ACCESS_TOKEN_SECRET = ""
 
 # The full url to an HTTPS proxy to use for connecting to Twitter.
 PROXY = ""
--- analyzer/config_feature_words.py
+++ analyzer/config_feature_words.py
@@ -8,10 +8,10 @@ DB_NAME = "twitter-archive"
 COLLECTION_NAME = "tweets"
 
 # twitter
-CONSUMER_KEY = "42ckSu4sLnYCLlzcLnQ0rEF6U"
-CONSUMER_SECRET = "VgKmmr1cJYjp6oWRIbQiV85cFIE494TdXRN0Gc6ebdkzNnpl7N"
-ACCESS_TOKEN_KEY = "615963673-vS0rtz54V4UxMeeWLq6EGi3vAO3fuN4cQUBfVHLe"
-ACCESS_TOKEN_SECRET = "sgUYRV7o9nD3dr4PzZ7wYc14n9BNpqzCUtqNjA7gKKNZP"
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN_KEY = ""
+ACCESS_TOKEN_SECRET = ""
 
 #分析するデータの日数
 ANALYZE_DAYS = 7
@@ -21,9 +21,9 @@ EXTRACT_FEATURE_WORDS_MAX = 10
 TFIDF_EXCLUDE_APPEARANCE = 5
 
 #出力先のフォルダのパス
-OUTPUT_FOLDER_PATH = "/Users/hitoshi/Dropbox/Public/tweet-analyzer/data/"
+OUTPUT_FOLDER_PATH = "../out/"
 
 # 日本語フォントのパス
 # OS Xなら/Library/Fonts, /System/Library/Fonts, ~/Library/Fontsあたりにあるはず。
 # EX: "/Library/Fonts/ヒラギノ丸ゴ ProN W4.ttc"
-JAPANESE_FONT_PATH = "/Library/Fonts/ヒラギノ丸ゴ ProN W4.ttc"
+JAPANESE_FONT_PATH = ""
