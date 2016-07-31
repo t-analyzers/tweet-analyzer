c5f1b2b0689df913b0c0ba335fdc4e7b79beef77
furuhit0519 <programming.adelie.penguin@gmail.com>
2016-07-31 21:37:50 +0900

    自動実行スクリプトでmongodbの開始と終了を制御するように変更。

--- twitter-analyzer.sh
+++ twitter-analyzer.sh
@@ -3,7 +3,10 @@
 export LC_LANG=ja_JP.UTF-8
 export LANG=ja_JP.UTF-8
 
-cd /Users/hitoshi/developer/tweet-analyzer-t/analyzer/
-#echo $PATH > ../logs/env.log
-/Users/hitoshi/.pyenv/versions/anaconda3-4.0.0/bin/python command.py a &> ../logs/archive.log
-/Users/hitoshi/.pyenv/versions/anaconda3-4.0.0/bin/python feature_words_extractor.py &> ../logs/feature_words_extractor.log
+/usr/local/bin/mongod --config /usr/local/etc/mongod.conf &
+
+cd $HOME/developer/tweet-analyzer-t/analyzer/
+$HOME/.pyenv/versions/anaconda3-4.0.0/bin/python command.py a &> ../logs/archive.log
+$HOME/.pyenv/versions/anaconda3-4.0.0/bin/python feature_words_extractor.py &> ../logs/feature_words_extractor.log
+
+exit
