36f1530c4dd43889240eff3acbf5f1a77b66d655
furuhit0519 <programming.adelie.penguin@gmail.com>
2017-01-08 12:26:34 +0900

    不要なパラメータを削除。

--- analyzer/config_metadata_api.py
+++ analyzer/config_metadata_api.py
@@ -13,5 +13,3 @@ NEGAPOSI_URL_ENDPOINT = "http://ap.mextractr.net/negaposi_measure"
 EMOTION_URL_ENDPOINT = "http://ap.mextractr.net/emotion_measure"
 KEY = "" #metadata株式会社のAPIキー。取得先：　http://www.metadata.co.jp/koseido-negapoji-api.html
 MAX_USAGE_COUNT = 100
-
-OUTPUT_FOLDER_PATH = "../out/"
